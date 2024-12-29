7d5e62e15760ae9cd26c5ef10dcc4db250e48a7b
조준민 <85844303+coom1222@users.noreply.github.com>
2024-12-30 02:42:41 +0900

    초기설정 셋팅

---
+++ Bangooseok/Gooseok-Cinema-BackEnd/src/test/java/Gooseok_Cinema/Gooseok_Cinema_BackEnd/GooseokCinemaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Gooseok_Cinema.Gooseok_Cinema_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooseokCinemaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
